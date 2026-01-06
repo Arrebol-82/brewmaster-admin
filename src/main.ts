33d70271355c1b7b8700f54a6434b3d68c12cfb8
Arrebol <13687403972@163.com>
2026-01-06 21:22:12 +0800

    feat(mock): initialize MSW and configure development data mocking

--- src/main.ts
+++ src/main.ts
@@ -9,19 +9,30 @@ import request from "./utils/request";
 import "./style.css";
 import App from "./App.vue";
 
-const app = createApp(App);
-// 创建应用实例 , APP 是根组件 , 所有的子组件都在 APP 组件内渲染
-
-// 注册 Pinia
-app.use(createPinia());
-
-// 注册 Element Plus
-app.use(ElementPlus);
-app.use(router);
-
 console.log("Axios instance loaded:", request);
+async function enableMocking() {
+  // 这里的 import.meta.env.DEV 是 Vite 提供的环境变量
+  if (import.meta.env.DEV) {
+    // 动态引入: 只有在开发时才会加载这个文件, 打包上线时不会把 mock 代码打包进去
+    const { worker } = await import("./mocks/browser");
+    // 启动 Service Worker
+    // worker.start()就是启动拦截器;
+    // onUnandledRequset: 'bypass' 意思是: 如果遇到没定义的接口,就直接放行 (不报错),让他走正常的请求流程
+    return worker.start({ onUnhandledRequest: "bypass" });
+  }
+}
 
-// 挂载应用
-app.mount("#app");
-/* 他告诉Vue把处理好的代码挂载到HTML 中 id 为 app 的那个容器里面, 所以你可以在根目录
+enableMocking().then(() => {
+  // 创建应用实例 , APP 是根组件 , 所有的子组件都在 APP 组件内渲染
+  const app = createApp(App);
+  // 注册 Pinia
+  app.use(createPinia());
+
+  // 注册 Element Plus
+  app.use(ElementPlus);
+  app.use(router);
+  // 挂载应用
+  app.mount("#app");
+  /* 他告诉Vue把处理好的代码挂载到HTML 中 id 为 app 的那个容器里面, 所以你可以在根目录
 的index.html 里面看到有一个 <div id="app"></div> 标签 */
+});
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,34 @@
+// http 是拦截器和监视器 , 用来拦截和监视URL
+// HttpResponse 用来模拟返回数据(响应体);
+import { http, HttpResponse } from "msw";
+export const handlers = [
+  // 1. 拦截 POST /api/login 请求
+  http.post("/api/login", () => {
+    console.log("MSW: 拦截成功!"); // 测试
+
+    //模拟返回一个成功的Token
+    // 这里 HttpResponse.json 是用来模拟返回数据(响应体);
+    return HttpResponse.json({
+      code: 200,
+      message: "登录成功 (Mock)",
+      data: {
+        token: "fake_token_123456", // 这是一个假 Token
+        username: "Arrebol",
+      },
+    });
+  }),
+
+  // 2. 拦截GET /api/me 请求 (获取用户信息)
+  http.get("/api/me", () => {
+    return HttpResponse.json({
+      code: 200,
+      message: "获取成功",
+      data: {
+        id: 1,
+        username: "admin",
+        role: "admin",
+        avatar: "https://api.dicebear.com/7.x/avataaars/svg?seed=old_boy",
+      },
+    });
+  }),
+];
